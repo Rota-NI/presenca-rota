6e686300dcc00193afb4a0d653e2d2063c42434f
Rota-NI <2021.1a2021.1@gmail.com>
2026-01-14 00:33:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,35 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 from datetime import datetime
-from fpdf import FPDF
 
 # --- CONFIGURAÇÃO DE ACESSO ---
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
-# O Streamlit vai ler as credenciais de um lugar seguro chamado "Secrets"
-try:
+# Função para conectar
+def conectar():
     creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
     client = gspread.authorize(creds)
-    spreadsheet = client.open("ListaPresenca")
-    sheet = spreadsheet.sheet1
-except Exception:
-    st.error("Aguardando configuração das chaves de acesso...")
+    return client.open("ListaPresenca").sheet1
 
-# --- TÍTULO DA PÁGINA ---
-st.markdown("<h1 style='text-align: center;'>🚌 ROTA NOVA IGUAÇU</h1>", unsafe_allow_html=True)
+# --- TÍTULO ---
+st.title("🚌 ROTA NOVA IGUAÇU")
 
-# --- FORMULÁRIO ---
-with st.form("meu_formulario", clear_on_submit=True):
-    nome = st.text_input("Nome de Escala:")
-    submit = st.form_submit_button("SALVAR PRESENÇA")
+try:
+    sheet = conectar()
     
-    if submit:
-        if nome:
-            agora = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-            sheet.append_row([agora, nome])
-            st.success(f"Presença de {nome} registrada!")
-        else:
-            st.error("Digite seu nome.")
+    # --- FORMULÁRIO ---
+    with st.form("meu_formulario", clear_on_submit=True):
+        nome = st.text_input("Nome de Escala:")
+        submit = st.form_submit_button("SALVAR PRESENÇA")
+        
+        if submit:
+            if nome:
+                agora = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+                sheet.append_row([agora, nome])
+                st.success(f"Presença de {nome} registrada!")
+            else:
+                st.error("Digite seu nome.")
+
+except Exception as e:
+    st.error(f"Erro de conexão: {e}")
+    st.info("Verifique se você compartilhou a planilha com o e-mail da conta de serviço.")
